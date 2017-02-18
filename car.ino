4e806d9809c75d3d93a9464d59ed8155b331025c
Zoe Demertzis <zed@knighscope.com>
2017-02-18 22:51:08 +0000

    initialize motors and begin driving logic, demo shown 2/16

--- car.ino
+++ car.ino
@@ -6,11 +6,12 @@
 //////////////////////////////////////
 // SET THESE TO YOUR AP CREDENTIALS //
 //////////////////////////////////////
-const char* ssid = "....";
-const char* password = "....";
+const char* ssid = "Zoe's iPhone";
+const char* password = "b95xxiywmn9l";
 
 const char* host = "jeremysorensen.site";
 const int httpPort = 80;
+char orientation = 'E';
 
 String message; // the message the car writes
 
@@ -51,11 +52,188 @@ String getRequest(String url) {
 ////////////////////////////////
 // TODO implement writing !!! //
 ////////////////////////////////
+
+void stop(void)
+{
+    analogWrite(5, 0);
+    analogWrite(4, 0);
+}
+
+void orientLeft(){
+  Serial.println("before left");
+  Serial.println(orientation);
+  if(orientation == 'N'){
+    orientation = 'W';
+  }else if(orientation == 'E'){
+    orientation = 'N';  
+  }else if(orientation == 'S'){
+    orientation = 'E';
+  }else{
+    orientation = 'S';
+  }
+  Serial.println("after left");
+  Serial.println(orientation);
+  
+}
+void orientRight(){
+  Serial.println("before right");
+  Serial.println(orientation);
+  if(orientation == 'N'){
+    orientation = 'E';
+  }else if(orientation == 'E'){
+    orientation = 'S';  
+  }else if(orientation == 'S'){
+    orientation = 'W';
+  }else{
+    orientation = 'N';
+  }
+  Serial.println("after right");
+  Serial.println(orientation);
+}
+
+void oneTick(){
+    Serial.println("forward");
+    analogWrite(5, 1023);
+    analogWrite(4, 1023);
+    digitalWrite(0, HIGH);
+    digitalWrite(2, HIGH);
+    delay(200);
+    stop();
+    analogWrite(5, 1023);
+    analogWrite(4, 1023);
+    digitalWrite(0, HIGH);
+    digitalWrite(2, HIGH);
+    delay(200);
+    stop();
+}
+
+void left(){
+  Serial.println("left");
+  analogWrite(5, 1023);
+  analogWrite(4, 1023);
+  digitalWrite(0, LOW);
+  digitalWrite(2, HIGH);
+  orientLeft();
+  delay(500);
+  stop();
+  
+}
+void right(){
+  Serial.println("right");
+  analogWrite(5, 1023);
+  analogWrite(4, 1023);
+  digitalWrite(0, HIGH);
+  digitalWrite(2, LOW);
+  orientRight();
+  delay(500);
+  stop();
+}
+
+void transition(){
+  //assume robot ends facing east in prev number
+  if(orientation == 'E'){
+    //pen up
+    oneTick();
+  }else{
+    Serial.println("ERROR robot cannot transition");
+  }
+}
+ 
+void one(void)
+{
+    right();
+    //lower pen
+    oneTick();
+    oneTick();
+    //raise pen
+    right();
+    right();
+    oneTick();
+    oneTick();
+    right();
+    transition();  
+}
+ 
+void two(void)
+{
+    //lower pen
+    oneTick();
+    right();
+    oneTick();
+    right();
+    oneTick();
+    left();
+    oneTick();
+    left();
+    oneTick();
+    //raise pen
+    left();
+    oneTick();
+    oneTick();
+    right();
+    transition();
+}
+ 
+void three(void)
+{
+  //lower pen
+  oneTick();
+  right();
+  oneTick();
+  right();
+  oneTick();
+  left();
+  left();
+  oneTick();
+  right();
+  oneTick();
+  right();
+  oneTick();
+  //raise pen
+  right();
+  oneTick();
+  oneTick();
+  right();
+  oneTick();
+  transition();
+   
+}
+ 
+void four(void)
+{
+  right();
+  //lower pen
+  oneTick();
+  left();
+  oneTick();
+  right();
+  oneTick();
+  left();
+  left();
+  oneTick();
+  oneTick();
+  right();
+  transition();   
+}
 void drawMessage() {
   message = getRequest("/tracerracer/car.php");
-  Serial.println("##########################################");
+  String newMessage = message.substring(message.length()-1);
+  Serial.println("begin##########################################");
   Serial.println(message);
-  Serial.println("##########################################");
+  Serial.println("just the char I want");
+  Serial.println(newMessage);
+  Serial.println("end##########################################");
+  if(newMessage == "1"){
+    one();
+  }else if(newMessage == "2"){
+    two();
+  }else if(newMessage == "3"){
+    three();
+  }else if(newMessage == "4"){
+    four();
+  }
+  Serial.println(orientation);
+  
 }
 
 ///////////////////////////////////////
@@ -119,6 +297,7 @@ void connectServer() {
 
   server.begin();
   Serial.println("HTTP server started");  
+  
 }
 
 void setup(void){
@@ -126,6 +305,11 @@ void setup(void){
   connectWifi();
   initCar();
   connectServer();
+  
+  pinMode(5, OUTPUT); // 1,2EN aka D1 pwm left
+  pinMode(4, OUTPUT); // 3,4EN aka D2 pwm right
+  pinMode(0, OUTPUT); // 1A,2A aka D3
+  pinMode(2, OUTPUT); // 3A,4A aka D4
 }
 
 void loop(void){
